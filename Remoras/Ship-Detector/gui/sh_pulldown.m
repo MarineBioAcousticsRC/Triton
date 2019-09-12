984e3c54b60163f257671536c80349c8921bfdec
Alba Solsona <alba.solb@gmail.com>
2019-09-11 17:39:43 -0700

    Plot labels GUI

--- Remoras/Ship-Detector/gui/sh_pulldown.m
+++ Remoras/Ship-Detector/gui/sh_pulldown.m
@@ -11,8 +11,9 @@ global PARAMS REMORA HANDLES
 
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-% dialog box - interactive spectrogram detector
+
 if strcmp(action, 'interactive')
+    % dialog box - interactive spectrogram detector
     if isempty(PARAMS.ltsa.infile)
         % no data loaded, display message to load data.
         disp_msg('Please load ltsa file before launching detector gui')
@@ -48,46 +49,44 @@ elseif strcmp(action,'full_detector')
     
     sh_setpointers('arrow');
 
-% elseif strcmp(action,'create_labels')
-%     ship_setpointers('watch');
-%     ship_setpointers('arrow');
+elseif strcmp(action,'create_labels')
+    sh_setpointers('watch');
+    
+    
+    
+    sh_setpointers('arrow');
     
 elseif strcmp(action,'load_labels')
-    [basename, path] = uigetfile('*.tlab', 'Set detection label file');
-    % if canceled button pushed:
-    if strcmp(num2str(basename),'0')
-        return
-    end
-    file = fullfile(path, basename);
-    if ~ exist(file,'file')
-        disp_msg(sprintf('Detection file %s does not exist', file));
-    else
-        [Starts, Stops, Labels] = sh_read_TLABFile(file, 'Binary', true);
-        REMORA.sh.class.starts = Starts;
-        REMORA.sh.class.stops = Stops;
-        REMORA.sh.class.labels = Labels;
-        REMORA.sh.class.files = {file}; % May want to add display
-                                        % filename later on...
-        REMORA.sh.class.ValidLabels = true;
-        REMORA.sh.class.PlotLabels = true; % Assume they want to see them.
-%         set(HANDLES.labelplot, 'Checked', 'on');
-        disp_msg(sprintf('Detection file %s read', file));
-    end
-    plot_triton;        % Replot showing labels
+    
+    % set up to open gui window for motion detector
+    sh_init_visLabel_figure
+    
+    % set motion gui for detection lables
+    sh_init_visLabel_gui
+    
+    
+% %     [basename, path] = uigetfile('*.tlab', 'Set detection label file');
+% %     % if canceled button pushed:
+% %     if strcmp(num2str(basename),'0')
+% %         return
+% %     end
+% %     file = fullfile(path, basename);
+% %     if ~ exist(file,'file')
+% %         disp_msg(sprintf('Detection file %s does not exist', file));
+% %     else
+% %         [Starts, Stops, Labels] = sh_read_TLABFile(file, 'Binary', true);
+% %         REMORA.sh.class.starts = Starts;
+% %         REMORA.sh.class.stops = Stops;
+% %         REMORA.sh.class.labels = Labels;
+% %         REMORA.sh.class.files = {file}; % May want to add display
+% %                                         % filename later on...
+% %         REMORA.sh.class.ValidLabels = true;
+% %         REMORA.sh.class.PlotLabels = true; % Assume they want to see them.
+% % %         set(HANDLES.labelplot, 'Checked', 'on');
+% %         disp_msg(sprintf('Detection file %s read', file));
+% %     end
+% %     plot_triton;        % Replot showing labels
 
-elseif strcmp(action, 'display_labels')
-    if REMORA.sh.class.ValidLabels
-        % toggle plot status & flag
-%         if REMORA.sh.class.PlotLabels
-%             set(HANDLES.labelplot, 'Checked', 'off');
-%         else
-%             set(HANDLES.labelplot, 'Checked', 'on');
-%         end
-        REMORA.sh.class.PlotLabels = ~ REMORA.sh.class.PlotLabels;
-        plot_triton;   % Replot with/without labels
-    else
-        sh_pd('load_labels');        % No valid label set, ask for one
-    end
 elseif strcmp(action,'evaluate_detections')
     warning('Under construction')
 end
