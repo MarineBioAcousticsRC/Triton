486833ad7f2596a438d83c1a9fb939d6dfada5c2
kfrasier <kaitfrasier@gmail.com>
2019-10-09 16:30:52 -0700

    bug fix on max ICI in cluster_bins

--- Remoras/ClusterTool/gui/ct_cb_control.m
+++ Remoras/ClusterTool/gui/ct_cb_control.m
@@ -101,7 +101,7 @@ elseif strcmp(action,'setEndFreq')
     
 elseif strcmp(action,'setBarIntMax')
     barIntMax = str2double(get(REMORA.ct.CB_verify.barIntMaxEdTxt, 'string'));
-    REMORA.ct.CB_params.rmEchos = barIntMax;
+    REMORA.ct.CB_params.barIntMax = barIntMax;
     
 elseif strcmp(action,'setInDir')
     inDir = get(REMORA.ct.CB_verify.inDirEdTxt, 'string');
