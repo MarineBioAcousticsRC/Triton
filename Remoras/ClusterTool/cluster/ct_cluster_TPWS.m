747dacd02527019972d682d43fd845ac54c3b6e7
kfrasier <kaitfrasier@gmail.com>
2019-10-11 16:36:48 -0700

    Added code to estimate f if not included in TPWS
    Also checks orientation of input matrices

--- Remoras/ClusterTool/cluster/ct_cluster_TPWS.m
+++ Remoras/ClusterTool/cluster/ct_cluster_TPWS.m
@@ -32,13 +32,33 @@ MSP = [];
 f = [];
 
 load(thisFile,'MPP','MTT','MSP','MSN','f')
-
+if size(MSP,1) ~= size(MSN,1)
+    
+    % if these don't have the same vertical dimension, but they are
+    % the same in the horizontal, assume they are
+    % oriented wrongly (columns represent individual detections instead of
+    % rows representing detections) and flip them.
+    if size(MSP,2) == size(MSN,2)
+        MSP = MSP';
+        MSN = MSN';
+    else
+        error('Error: MSP and MSN don''t have the same dimensions. Each detection should be on one row.')
+    end
+end
+    
 if ~isempty(f)
     fkeep = f;
 elseif isempty(f) && ~isempty(fkeep)
     f = fkeep;
+elseif isfield(p,'sampleRate')
+    warning(['Warning: Missing frequency vector in input file. ',...
+         'Estimating using sample rate and some possibly incorrect assumptions.'])
+    sizeMSP2 = size(MSP,2);% this is dangerous, MSP could be flipped.
+    
+    f = ((p.sampleRate/2)/(sizeMSP2-1))*(1:sizeMSP2) -(p.sampleRate/2)/(sizeMSP2-1);
 else
-    error('Error: Missing frequency vector in input file.')
+    error(['Error: No frequency vector in input file, and no sample rate in parameters.',... 
+      'Please add f to TPWS or add sampleRate to settings file.'])
 end
 
 % Turn specified frequencies into indices
