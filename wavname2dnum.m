ee40145e8e03502ccd4b89662da0724148c672c1
Alba Solsona <alba.solb@gmail.com>
2019-11-13 14:29:37 -0800

    Read datetimes yymmddHHMMSS first to avoid reading this format as a d{6}

--- wavname2dnum.m
+++ wavname2dnum.m
@@ -17,6 +17,14 @@ filenamesc = cellstr(filenames);
 date_strs = regexp(filenamesc,'\d{6}[-]\d{6}','match'); 
 date_fmt = 'yymmdd-HHMMSS';
 
+if isempty(date_strs{1}) % not a PAMguard file, try avisoft filename
+    date_fmt = 'yymmddHHMMSS';
+    date_strs = regexp(filenamesc,'\d{12}','match' );
+    if ~isempty(date_strs{1})
+        disp('Using avisoft and SoundTrap filename format yymmddHHMMSS');
+    end
+end
+
 if isempty(date_strs{1}) % not just and underscore problem, try PAMGuard filename
     date_fmt = 'yyyymmdd_HHMMSS'; % PAMGuard default file format 
     date_strs = regexp(filenamesc,'\d{8}[_]\d{6}','match');
@@ -30,14 +38,6 @@ if isempty(date_strs{1}) % using underscores presumably
     date_strs = regexp(filenamesc,'\d{6}[_]\d{6}','match');
 end
 
-
-if isempty(date_strs{1}) % not a PAMguard file, try avisoft filename
-    date_fmt = 'yymmddHHMMSS';
-    date_strs = regexp(filenamesc,'\d{12}','match' );
-    if ~isempty(date_strs{1})
-        disp('Using avisoft filename format yymmddHHMMSS');
-    end
-end
 
 if isempty(date_strs{1})
     disp('Unknown filename date format.  Please use one of the following:');
