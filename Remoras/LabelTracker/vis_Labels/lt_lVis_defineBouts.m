b4c51d2888a390cbabc9898c6c053e62a6bfd350
MZiegenhorn <40473398+MZiegenhorn@users.noreply.github.com>
2020-04-14 13:02:10 -0700

    fix some bugs, speed up LTSA plotting

--- Remoras/LabelTracker/vis_Labels/lt_lVis_defineBouts.m
+++ Remoras/LabelTracker/vis_Labels/lt_lVis_defineBouts.m
@@ -1,6 +1,10 @@
 function [boutStarts,boutStops] = lt_lVis_defineBouts(starts,stops,boutGap)
 %function for creating bout-level times from click detections 
 
+%if starts and stops are not chronological, reorder
+starts = sort(starts);
+stops = sort(stops);
+
 nextDet = [starts(2:end);stops(end)];
 detDiff = nextDet - stops;
 
--- Remoras/LabelTracker/vis_Labels/lt_lVis_get_LTSA_Offset.m
+++ Remoras/LabelTracker/vis_Labels/lt_lVis_get_LTSA_Offset.m
@@ -6,36 +6,31 @@ rfDur = PARAMS.ltsa.dur(1); %assumes same duration of all rfs in LTSA
 
 if strcmp(action,'starts')
     for iWin = 1:size(inWindowDet,1)
-        winDetWavIdx = find(PARAMS.ltsa.dnumStart<=inWindowDet(iWin,1)& inWindowDet(iWin,1)<=PARAMS.ltsa.dnumEnd);
-        if ~isempty(winDetWavIdx)
-            winDetWavs = PARAMS.ltsa.dnumStart(winDetWavIdx);
-            detstartOff = (winDetWavIdx - plotStart).*rfDur; %find offset of raw file from start of LTSA
-            detWavOff = inWindowDet(iWin,1) - winDetWavs;
-            detWavOffSec = lt_convertDatenum(detWavOff,'seconds');
-            detXpos(iWin) = (detstartOff + detWavOffSec)./3600; %convert seconds back to hours
-        else
-            detXpos(iWin) = 0; %skip weird detection
-            disp('detection found not within any raw file!')
-        end
+        winDetWavIdx{iWin} = find(PARAMS.ltsa.dnumStart<=inWindowDet(iWin,1)& inWindowDet(iWin,1)<=PARAMS.ltsa.dnumEnd);
     end
+    
+    wDWIdx = [winDetWavIdx{:}];
+    winDetWavs = PARAMS.ltsa.dnumStart(wDWIdx)';
+    detstartOff = (wDWIdx - plotStart).*rfDur; %find offset of raw file from start of LTSA
+    detWavOff = inWindowDet(:,1) - winDetWavs;
+    detWavOffSec = lt_convertDatenum(detWavOff,'seconds');
+    detXpos = (detstartOff' + detWavOffSec)./3600; %convert seconds back to hours
     
     
     
 elseif strcmp(action,'stops')
     
     for iWin = 1:size(inWindowDet,1)
-        winDetWavIdx = find(PARAMS.ltsa.dnumStart<=inWindowDet(iWin,2)& inWindowDet(iWin,2)<=PARAMS.ltsa.dnumEnd);
-        if ~isempty(winDetWavIdx) %deal with odd anomaly detections
-            winDetWavs = PARAMS.ltsa.dnumStart(winDetWavIdx);
-            detstartOff = (winDetWavIdx - plotStart).*rfDur; %find offset of raw file from start of LTSA
-            detWavOff = inWindowDet(iWin,2) - winDetWavs';
-            detWavOffSec = lt_convertDatenum(detWavOff,'seconds');
-            detXpos(iWin) = (detstartOff' + detWavOffSec)./3600; %convert seconds over to hours
-        else
-            detXpos(iWin) = 0; %skip weird detection
-            disp('detection found not within any raw file!')
-        end
+        winDetWavIdx{iWin} = find(PARAMS.ltsa.dnumStart<=inWindowDet(iWin,2)& inWindowDet(iWin,2)<=PARAMS.ltsa.dnumEnd);
     end
+    
+    wDWIdx = [winDetWavIdx{:}];
+    winDetWavs = PARAMS.ltsa.dnumStart(wDWIdx)';
+    detstartOff = (wDWIdx - plotStart).*rfDur; %find offset of raw file from start of LTSA
+    detWavOff = inWindowDet(:,2) - winDetWavs;
+    detWavOffSec = lt_convertDatenum(detWavOff,'seconds');
+    detXpos = (detstartOff' + detWavOffSec)./3600; %convert seconds back to hours
+    
 else
     disp('WARNING! Action not registered. Available options are starts or stops')
 end
