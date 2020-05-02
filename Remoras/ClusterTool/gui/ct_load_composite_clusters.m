a7998dfbae5144d3cf1e70782c716ef911b2c866
kfrasier <kaitfrasier@gmail.com>
2020-05-01 18:29:38 -0700

    bug fixes for partition loading

--- Remoras/ClusterTool/gui/ct_load_composite_clusters.m
+++ Remoras/ClusterTool/gui/ct_load_composite_clusters.m
@@ -6,7 +6,13 @@ disp('loading...')
 ccData = load(fullfile(PathName,FileName));
 % TODO: need some kind of check here to see if it's the right thing.
 REMORA.ct.CC.output = ccData;
-REMORA.ct.CC_params = ccData.p;
+REMORA.ct.CC_params = ccData.s;
 REMORA.ct.CC_params.outputName = FileName;
 REMORA.ct.CC.output.remakePlots = 1;
+REMORA.ct.CC.output.s.saveOutput = 0;
+ct_intercluster_plots(REMORA.ct.CC.output.p,REMORA.ct.CC.output.s,...
+            REMORA.ct.CC.output.f,REMORA.ct.CC.output.nodeSet,...
+            REMORA.ct.CC.output.compositeData,REMORA.ct.CC.output.Tfinal,...
+            REMORA.ct.CC.output.labelStr,[]);
+
 disp('load complete.')
