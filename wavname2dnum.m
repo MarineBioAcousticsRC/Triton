2bd2acb869c41395ac0d41d13e05f5253153f703
sfregosi <22456126+sfregosi@users.noreply.github.com>
2020-06-30 18:35:45 -0400

    added new filename format - yyyymmddTHHMMSS - added to deal with AMAR files

--- wavname2dnum.m
+++ wavname2dnum.m
@@ -1,11 +1,12 @@
 function [ dnums ] = wavname2dnum( filenames )
 % Parses .wav file names and converts them to matlab datenums
 % Works on single or multiple file names
-% Supports 4 filename formats:
+% Supports 5 filename formats:
 %   1.  yymmdd-HHMMSS
 %   2.  yymmdd_HHMMSS
 %   3.  yyyymmdd_HHMMSS 
 %   4.  yymmddHHMMSS
+%   5.  yyyymmddTHHMMSS added for AMAR613.20190604T182000Z.wav
 
 % This matches both underscores and hyphens, but don't know how to handle
 % the datenum formatting...maybe useful later?
@@ -36,6 +37,15 @@ if isempty(date_strs{1}) % not a PAMguard file, try avisoft filename
     date_strs = regexp(filenamesc,'\d{12}','match' );
     if ~isempty(date_strs{1})
         disp('Using avisoft filename format yymmddHHMMSS');
+    end
+end
+
+% added 2020 06 26 by S. Fregosi
+if isempty(date_strs{1}) % try AMAR filename - e.g., AMAR613.20190604T182000Z.wav
+    date_fmt = 'yyyymmddTHHMMSS';
+    date_strs = regexp(filenamesc,'\d{8}[T]\d{6}','match');
+    if ~isempty(date_strs{1})
+        disp('Using AMAR filename format yyyymmddTHHMMSS');
     end
 end
 
