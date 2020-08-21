a5316e9d691299424052875d71c8e4fd2a004783
sfregosi <22456126+sfregosi@users.noreply.github.com>
2020-08-21 14:14:50 -0400

    updated version displayed at startup

--- triton.m
+++ triton.m
@@ -30,8 +30,8 @@ warning off % this is turned off for plotting messages
 
 global PARAMS
 
-PARAMS.ver = '1.93.20160524';
-
+% PARAMS.ver = '1.93.20160524';
+PARAMS.ver = '1.0 2020 08 21 SF';
 disp(' ')
 disp(['         Triton version ',PARAMS.ver])
 
