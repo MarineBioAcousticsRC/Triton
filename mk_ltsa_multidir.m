b90cd7692ab241ac65e050d36778cf6e74ec8da2
sfregosi <22456126+sfregosi@users.noreply.github.com>
2021-06-01 12:24:31 -0400

    toggle disp message for wavname2dnum off when using get_headers because it would disp 1000s of lines and the original mk ltsa messages would get lost

--- mk_ltsa_multidir.m
+++ mk_ltsa_multidir.m
@@ -1,11 +1,11 @@
 function mk_ltsa_multidir()
 
-global PARAMS REMORA 
+global PARAMS REMORA
 
 PARAMS.ltsa.multidir = 1;
 
 % if we're coming from the processing code, just use the LTSA data from
-% that. if not, use user input 
+% that. if not, use user input
 % also can be used with other predefined LTSA processing information
 if isfield(REMORA, 'hrp')
     indirs = {};
@@ -44,52 +44,59 @@ else
     
     % save output files in same locations
     outdirs = indirs;
-
+    
     % LTSA parameters
-    ltsa_params(indirs);
+    ltsa_params(indirs); % set taves and dfreqs
     taves = PARAMS.ltsa.taves;
     dfreqs = PARAMS.ltsa.dfreqs;
     
-%     taves = 5; 
-%     dfreqs = [100, 1, 10];
+    %     taves = 5;
+    %     dfreqs = [100, 1, 10];
     
     PARAMS.ltsa.ch = 1; % which channel do you want to process?
-%     PARAMS.ltsa.ftype = 2; % 2 for xwavs, 1 for wavs
-    PARAMS.ltsa.dtype =  1; % 1 for HRP data
-
+    %     PARAMS.ltsa.ftype = 2; % 2 for xwavs, 1 for wavs
+    if PARAMS.ltsa.ftype == 2
+        % specify data type -1 HRP, 2 ARP, 3 OBS? Pulling these nums from
+        % ck_ltsaparams.m
+        PARAMS.ltsa.dtype =  1; % 1 for HRP data
+    else % all wav files? Maybe not...but leaving for now.
+        PARAMS.ltsa.dtype = 4; % standard wav/ishmael format?
+    end
+    
     % raw files to skip. leave this empty if no rfs wanted to skip
     %PARAMS.ltsa.rf_skip = [47957  47986  47989  48016  48019  48045  48048  48051  48081  48541];
-    PARAMS.ltsa.rf_skip = [11716  11715];
+    %     PARAMS.ltsa.rf_skip = [11716  11715];
+    PARAMS.ltsa.rf_skip = [];
     
-    % loop through each of the sets of directories for pre-check
+    % loop through each of the sets of directories for PRE-CHECK
     prefixes = {1, length(indirs)};
-    outfiles = {1, length(indirs)}; 
+    outfiles = {1, length(indirs)};
     dirdata = {1, length(indirs)};
     for k = 1:length(indirs)
         % if we have different parameters for each of the dirs, adjust
         % accordingly
         if length(dfreqs) > 1
             dfreq = dfreqs(k);
-        else 
+        else
             dfreq = dfreqs;
         end
         if length(taves) > 1
             tave = taves(k);
-        else 
+        else
             tave = taves;
         end
-
+        
         PARAMS.ltsa.indir = char(indirs{k});
         PARAMS.ltsa.outdir = char(outdirs{k});
         PARAMS.ltsa.tave = tave;
         PARAMS.ltsa.dfreq = dfreq;
-
+        
         % create the outfile and prefix
         [prefixes{k}, outfiles{k}, dirdata{k}] = gen_prefix();
-
+        
         % make sure filenames will work
         success = ck_names(prefixes{k});
-
+        
         % check to see if the ltsa file already exists
         PARAMS.ltsa.indir = indirs{k};
         if exist(fullfile(PARAMS.ltsa.indir, outfiles{k}), 'file')
@@ -98,7 +105,7 @@ else
             if strcmp(choice, 'Continue, don''t overwrite')
                 outfiles{k} = sprintf('%s_copy.ltsa', PARAMS.ltsa.outfile(1:end-5));
                 ltsa_bools(k) = 1;
-            % remove parameters for this LTSA
+                % remove parameters for this LTSA
             elseif strcmp(choice, 'Skip')
                 indirs(k) = [];
                 outdirs(k) = [];
@@ -106,11 +113,11 @@ else
                 outfiles(k) = [];
                 dirdata(k) = [];
                 taves(k) = [];
-                dfreqs(k) = []; 
-            end    
+                dfreqs(k) = [];
+            end
         end
         
-%         if strcmp(ans, 'Skip') || ~success
+        %         if strcmp(ans, 'Skip') || ~success
         if ~success
             disp_msg(sprintf('Skipping LTSA creation for %s\n', prefixes{k}));
             indirs(k) = [];
@@ -119,14 +126,14 @@ else
             outfiles(k) = [];
             dirdata(k) = [];
             taves(k) = [];
-            dfreqs(k) = []; 
+            dfreqs(k) = [];
         end
     end
 end
 
 % loop through each of the sets of directories for actual ltsa creation
 for k = 1:length(indirs)
-  
+    
     % if we have different parameters for each of the dirs, adjust
     % accordingly
     if length(taves) > 1
@@ -136,176 +143,178 @@ for k = 1:length(indirs)
     end
     if length(dfreqs) > 1
         dfreq = dfreqs(k);
-    else 
+    else
         dfreq = dfreqs;
     end
-
+    
     PARAMS.ltsa.indir = char(indirs{k});
     PARAMS.ltsa.outdir = char(outdirs{k});
     PARAMS.ltsa.outfile = char(outfiles{k});
     PARAMS.ltsa.tave = tave;
     PARAMS.ltsa.dfreq = dfreq;
-
-%     prefix = prefixes{k};
-
+    
+    %     prefix = prefixes{k};
+    
     % run from matlab command line
     if ~isfield(REMORA, 'hrp')
         d = dirdata{k};
-        if ~isfield(d, 'dataID')
+        if ~isfield(d, 'dataID') % non xwav/typical dir
             fprintf('\nMaking LTSA for directory %s\n', PARAMS.ltsa.indir)
         else
             fprintf('\nMaking LTSA for %s disk %s df %i\n', d.dataID, d.disk, d.df);
         end
-    % run from procFun
+        % run from procFun
     else
-        fprintf('\nMaking LTSA for %s disk %s df %i\n', REMORA.hrp.dataID, REMORA.hrp.disk, REMORA.hrp.dfs(k));  
+        fprintf('\nMaking LTSA for %s disk %s df %i\n', REMORA.hrp.dataID, REMORA.hrp.disk, REMORA.hrp.dfs(k));
     end
-
+    
     % make the ltsa!
     mk_ltsa_dir;
-end
+    fprintf('\nFinished LTSA for directory %s\n Press any key to continue...\n', PARAMS.ltsa.indir)
+    pause
+end % all directories
 
 end
 
 % check to see if the xwav/wav names are compatible with ltsa format
 function success = ck_names(prefix)
 
-    global PARAMS
-    
-    success = 1;
-    
-    % find filenames for xwav/wav
-    if PARAMS.ltsa.ftype == 1
-        files = dir(fullfile(PARAMS.ltsa.indir, '*.wav'));
-    elseif PARAMS.ltsa.ftype == 2
-        files = dir(fullfile(PARAMS.ltsa.indir, '*.x.wav'));
+global PARAMS
+
+success = 1;
+
+% find filenames for xwav/wav
+if PARAMS.ltsa.ftype == 1
+    files = dir(fullfile(PARAMS.ltsa.indir, '*.wav'));
+elseif PARAMS.ltsa.ftype == 2
+    files = dir(fullfile(PARAMS.ltsa.indir, '*.x.wav'));
+end
+
+% check to see if filenames are the same length
+prev_fname = files(1).name;
+for x = 1:length(files)
+    curr_fname = files(x).name;
+    if length(prev_fname) ~= length(curr_fname)
+        success = 0;
+        break;
     end
-       
-    % check to see if filenames are the same length
-    prev_fname = files(1).name;
-    for x = 1:length(files)
-        curr_fname = files(x).name;
-        if length(prev_fname) ~= length(curr_fname)
-            success = 0;
-            break;
-        end
-    end
+end
+
+% check to see if filenames contain timestamps (wavs only! xwavs have
+% timing info in headers)
+if PARAMS.ltsa.ftype == 1
+    fname = files(1).name;
+    dnum = wavname2dnum(fname);
     
-    % check to see if filenames contain timestamps (wavs only! xwavs have
-    % timing info in headers)
-    if PARAMS.ltsa.ftype == 1
-        fname = files(1).name;
-        dnum = wavname2dnum(fname);
-    
-        % datenumber isn't in filenames
-        if isempty(dnum); success = 0; end
-    end
-    
-    % if we don't like the format, offer to change wav/xwav filenames
-    if ~success; success = rename_wavs(prefix); end
-    
-    % either continue with ltsa creation or return
-    if success; success = 1; return; end;        
+    % datenumber isn't in filenames
+    if isempty(dnum); success = 0; end
+end
+
+% if we don't like the format, offer to change wav/xwav filenames
+if ~success; success = rename_wavs(prefix); end
+
+% either continue with ltsa creation or return
+if success; success = 1; return; end;
 end
 
 % create a prefix and an outfile name for ltsas generated
 function [prefix,ltsa_file,dirdat] = gen_prefix()
-    global PARAMS
+global PARAMS
 
-    % struct to hold dataID, disk #, and df of this indir
-    dirdat = struct; 
-    
-    % check for typical xwav header format
-    [~, dirname, ~] = fileparts(PARAMS.ltsa.indir);
-    exp = '^[\w-_]+(?=_disk)';
-    dataID = regexp(dirname, exp, 'match');
-    
-     % traditional xwav/wav filename format
-     if ~isempty(dataID)
-        dataID = char(dataID(1,:));
+% struct to hold dataID, disk #, and df of this indir
+dirdat = struct;
 
-        exp = 'disk(\d{2})[_df]*([0-9]*)';
-        tokens = regexp(dirname, exp, 'tokens');
-        tokens = tokens{1};
+% check for typical xwav header format
+[~, dirname, ~] = fileparts(PARAMS.ltsa.indir);
+exp = '^[\w-_]+(?=_disk)';
+dataID = regexp(dirname, exp, 'match');
 
-        disk = tokens{1};
-        
-        % create the prefix
-        if ~isempty(tokens{2})
-            df = str2num(tokens{2});
-            prefix = ...
-                sprintf('%s_disk%s',dataID,disk); 
-            ltsa_file = ...
-                sprintf('%s_%ds_%dHz.ltsa',prefix,PARAMS.ltsa.tave,PARAMS.ltsa.dfreq,df);
-        else
-            df = 1;
-            prefix = ...
-                sprintf('%s_disk%s',dataID,disk);
-        end
-        
-        dirdat.dataID = dataID;
-        dirdat.disk = disk; 
-        dirdat.df = df;
-     
-     % if non-traditional format, use directory name to create prefix and
-     % ltsa name
-     else
-        % get parent directory name 
-        prefix = strsplit(PARAMS.ltsa.indir, '\'); % TODO issue between running on windows and mac?
-        prefix = prefix{end};
-        prefix = strrep(prefix, ' ', '_');
-        exp = '[^\w]+';
-        prefix = regexprep(prefix, exp, '');
-        prefix = strrep(prefix, '__', '_'); 
-     end
-     
-     % make sure that prefix isn't too long to fit into ltsa metadata
-     % (80 characters!)
-     if PARAMS.ltsa.ftype == 2 % xwavs
-         lim = 60;
-     elseif PARAMS.ltsa.ftype == 1 % wavs
-         lim = 62;
-     end
-     
-    if length(prefix) > lim
-        prefix = prefix(1:lim);
-    end 
+% traditional xwav/wav filename format
+if ~isempty(dataID)
+    dataID = char(dataID(1,:));
     
-    % create the outfile name
-    if ~strcmp(prefix(end), '_')
-        prefix = [prefix, '_'];
-    end  
-     
-     ltsa_file = ... 
-            sprintf('%s%ds_%dHz.ltsa',prefix,PARAMS.ltsa.tave,PARAMS.ltsa.dfreq);
+    exp = 'disk(\d{2})[_df]*([0-9]*)';
+    tokens = regexp(dirname, exp, 'tokens');
+    tokens = tokens{1};
+    
+    disk = tokens{1};
+    
+    % create the prefix
+    if ~isempty(tokens{2})
+        df = str2num(tokens{2});
+        prefix = ...
+            sprintf('%s_disk%s',dataID,disk);
+        ltsa_file = ...
+            sprintf('%s_%ds_%dHz.ltsa',prefix,PARAMS.ltsa.tave,PARAMS.ltsa.dfreq,df);
+    else
+        df = 1;
+        prefix = ...
+            sprintf('%s_disk%s',dataID,disk);
+    end
+    
+    dirdat.dataID = dataID;
+    dirdat.disk = disk;
+    dirdat.df = df;
+    
+    % if non-traditional format, use directory name to create prefix and
+    % ltsa name
+else
+    % get parent directory name
+    prefix = strsplit(PARAMS.ltsa.indir, '\'); % TODO issue between running on windows and mac?
+    prefix = prefix{end};
+    prefix = strrep(prefix, ' ', '_');
+    exp = '[^\w]+';
+    prefix = regexprep(prefix, exp, '');
+    prefix = strrep(prefix, '__', '_');
+end
+
+% make sure that prefix isn't too long to fit into ltsa metadata
+% (80 characters!)
+if PARAMS.ltsa.ftype == 2 % xwavs
+    lim = 60;
+elseif PARAMS.ltsa.ftype == 1 % wavs
+    lim = 62;
+end
+
+if length(prefix) > lim
+    prefix = prefix(1:lim);
+end
+
+% create the outfile name
+if ~strcmp(prefix(end), '_')
+    prefix = [prefix, '_'];
+end
+
+ltsa_file = ...
+    sprintf('%s%ds_%dHz.ltsa',prefix,PARAMS.ltsa.tave,PARAMS.ltsa.dfreq);
 end
 
 function dirs = find_dirs(d, ftype)
-    cd(d);
-    dirs = {};
-   
-    % find each of the subdirectories
-    files = dir; 
-    inds = find(vertcat(files.isdir));
-    subdirs = {};
-    subdirs_xwav = {};
-    for k = 1:length(inds)
-        ind = inds(k);
-        if ~strcmp(files(ind).name, '.') && ~strcmp(files(ind).name, '..')
-            subdirs{end+1} = fullfile(d, files(ind).name);
-        end
+cd(d);
+dirs = {};
+
+% find each of the subdirectories
+files = dir;
+inds = find(vertcat(files.isdir));
+subdirs = {};
+subdirs_xwav = {};
+for k = 1:length(inds)
+    ind = inds(k);
+    if ~strcmp(files(ind).name, '.') && ~strcmp(files(ind).name, '..')
+        subdirs{end+1} = fullfile(d, files(ind).name);
     end
-    
-    % for each subdirectory, check for xwavs and append to list of indirs
-    for k = 1:size(subdirs, 2)
-        subdirs_xwav = find_dirs(subdirs{k}, ftype);       
-        dirs = cat_cell(dirs, subdirs_xwav);
-    end
-    cd(d);
-    if ~isempty(dir(ftype))
-        dirs{end+1} = d;
-    end   
-    
+end
+
+% for each subdirectory, check for xwavs and append to list of indirs
+for k = 1:size(subdirs, 2)
+    subdirs_xwav = find_dirs(subdirs{k}, ftype);
+    dirs = cat_cell(dirs, subdirs_xwav);
+end
+cd(d);
+if ~isempty(dir(ftype))
+    dirs{end+1} = d;
+end
+
 end
 
 % concatenate two cell arrays cause APPARENTLY THIS ISN'T EASY IN MATLAB
@@ -316,7 +325,7 @@ for k = 1:size(c2, 2)
 end
 end
 
-function ltsa_params(dirs)
+function ltsa_params(dirs) % creates gui window to define ltsa settings
 mycolor = [.8,.8,.8];
 r = length(dirs) + 2;
 c = 3;
--- wavname2dnum.m
+++ wavname2dnum.m
@@ -6,18 +6,17 @@ function [dnums] = wavname2dnum(filenames, dispOn)
 %   2.  yymmdd_HHMMSS
 %   3.  yyyymmdd_HHMMSS 
 %   4.  yymmddHHMMSS
-%   5.  yyyymmddTHHMMSS
+%   5.  yyyymmddTHHMMSS added for AMAR613.20190604T182000Z.wav
 
 % This matches both underscores and hyphens, but don't know how to handle
 % the datenum formatting...maybe useful later?
 % regexp(fname,'\d{4}[-_]\d{4}','match','split')
 
-if nargin < 2
-    dispOn = 1; % added to limit printing of dnum format during LTSA creation
+if nargin <2
+    dispOn = 1;
 end
 
 % start with the default date format
-
 filenamesc = cellstr(filenames);
 date_strs = regexp(filenamesc,'\d{6}[-]\d{6}','match'); 
 date_fmt = 'yymmdd-HHMMSS';
@@ -26,23 +25,23 @@ date_fmt = 'yymmdd-HHMMSS';
 if isempty(date_strs{1})
     date_fmt = 'yymmddTHHMMSSZ';
     date_strs = regexp(filenamesc,'\d{6}[T]\d{6}[Z]','match' );
-    if ~isempty(date_strs{1})
+    if ~isempty(date_strs{1}) && dispOn == 1
         disp('Using ISO8601 date format yymmddTHHMMSSZ in filename');
     end
 end
 
-if isempty(date_strs{1}) % not a PAMguard file, try avisoft or Soundtrap filename
+if isempty(date_strs{1}) % not a PAMguard file, try avisoft filename
     date_fmt = 'yymmddHHMMSS';
     date_strs = regexp(filenamesc,'\d{12}','match' );
-    if ~isempty(date_strs{1})
+    if ~isempty(date_strs{1}) && dispOn == 1
         disp('Using avisoft and SoundTrap filename format yymmddHHMMSS');
     end
 end
 
-if isempty(date_strs{1}) % not just an underscore problem, try PAMGuard filename
+if isempty(date_strs{1}) % not just and underscore problem, try PAMGuard filename
     date_fmt = 'yyyymmdd_HHMMSS'; % PAMGuard default file format 
     date_strs = regexp(filenamesc,'\d{8}[_]\d{6}','match');
-    if ~isempty(date_strs{1})
+    if ~isempty(date_strs{1}) && dispOn == 1
         disp('Using PAMGuard filename format yyyymmdd_HHMMSS');
     end
 end
@@ -52,10 +51,11 @@ if isempty(date_strs{1}) % using underscores presumably
     date_strs = regexp(filenamesc,'\d{6}[_]\d{6}','match');
 end
 
+% added 2020 06 26 by S. Fregosi
 if isempty(date_strs{1}) % try AMAR filename - e.g., AMAR613.20190604T182000Z.wav
     date_fmt = 'yyyymmddTHHMMSS';
     date_strs = regexp(filenamesc,'\d{8}[T]\d{6}','match');
-    if ~isempty(date_strs{1})
+    if ~isempty(date_strs{1}) && dispOn == 1
         disp('Using AMAR filename format yyyymmddTHHMMSS');
     end
 end
