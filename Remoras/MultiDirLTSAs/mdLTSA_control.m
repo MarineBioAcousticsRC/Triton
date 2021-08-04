71b70599d9f5f69074143797d7f17ec6700ee2a5
sfregosi <22456126+sfregosi@users.noreply.github.com>
2021-08-04 03:20:57 -1000

    implemented radio buttons for data type

--- Remoras/MultiDirLTSAs/mdLTSA_control.m
+++ Remoras/MultiDirLTSAs/mdLTSA_control.m
@@ -18,67 +18,12 @@ elseif strcmp(action, 'browseInDir')
     dir = uigetdir();
     if ~ isnumeric(dir)
         % user selected something
+         set(REMORA.mdLTSA_verify.inDirEdTxt, 'string', dir);
         REMORA.mdLTSA.settings.inDir = dir;
     end
     
     
-    % 
-% %%
-% % --- Executes on button press in browse.
-% function browse_Callback(hObject, eventdata, handles)
-% % hObject    handle to browse (see GCBO)
-% % eventdata  reserved - to be defined in a future version of MATLAB
-% % handles    structure with handles and user data (see GUIDATA)
-% dir = uigetdir();
-% if ~ isnumeric(dir)
-%     % User selected something
-%     set(handles.(mfilename).Directory, 'String', dir);
-% end
 % 
-
-% elseif strcmp(action,'setOutDir')
-%     outDir = get(REMORA.bm_verify.outDirEdTxt, 'string');
-%     REMORA.bm.settings.outDir = outDir;
-%       
-% elseif strcmp(action,'setRegDate')
-%     regdate = get(REMORA.bm_verify.regdateEdTxt, 'string');
-%     REMORA.bm.settings.regdate = regdate;
-%     
-% elseif strcmp(action,'setThresh')
-%     ThreshEdText = str2double(get(REMORA.bm_verify.ThreshEdText, 'string'));
-%     REMORA.bm.settings.thresh = ThreshEdText;
-%     
-% elseif strcmp(action, 'setStartF1')
-%     startF1EdText = str2double(get(REMORA.bm_verify.StartF1EdText, 'string'));
-%     REMORA.bm.settings.startF(1,1) = startF1EdText;
-%     
-% elseif strcmp(action, 'setStartF2')
-%     startF2EdText = str2double(get(REMORA.bm_verify.StartF2EdText, 'string'));
-%     REMORA.bm.settings.startF(1,2) = startF2EdText;
-% 
-% elseif strcmp(action, 'setStartF3')
-%     startF3EdText = str2double(get(REMORA.bm_verify.StartF3EdText, 'string'));
-%     REMORA.bm.settings.startF(1,3) = startF3EdText;
-% 
-% elseif strcmp(action, 'setStartF4')
-%     startF4EdText = str2double(get(REMORA.bm_verify.StartF4EdText, 'string'));
-%     REMORA.bm.settings.startF(1,4) = startF4EdText;
-% 
-% elseif strcmp(action, 'setEndF1')
-%     endF1EdText = str2double(get(REMORA.bm_verify.EndF1EdText, 'string'));
-%     REMORA.bm.settings.endF(1,1) = endF1EdText;
-%     
-% elseif strcmp(action, 'setEndF2')
-%     endF2EdText = str2double(get(REMORA.bm_verify.EndF2EdText, 'string'));
-%     REMORA.bm.settings.endF(1,2) = endF2EdText;
-% 
-% elseif strcmp(action, 'setEndF3')
-%     endF3EdText = str2double(get(REMORA.bm_verify.EndF3EdText, 'string'));
-%     REMORA.bm.settings.endF(1,3) = endF3EdText;
-% 
-% elseif strcmp(action, 'setEndF4')
-%     endF4EdText = str2double(get(REMORA.bm_verify.EndF4EdText, 'string'));
-%     REMORA.bm.settings.endF(1,4) = endF4EdText;
 % 
 % elseif strcmp(action,'setSpecies')
 %     species.val = get(REMORA.bm_verify.SpeciesChoice, 'Value');
@@ -86,23 +31,18 @@ elseif strcmp(action, 'browseInDir')
 %     SpeciesChoice = species.opt{species.val};
 %     REMORA.bm.settings.species = SpeciesChoice;
 %     
-%     
-% elseif strcmp(action,'setSoundTrapdata')
-%     SoundTrapdataCheckbox = get(REMORA.bm_verify.SoundTrapdataCheckbox, 'string');
-%     REMORA.bm.settings.SoundTrap = SoundTrapdataCheckbox;
-%     
-% elseif strcmp(action,'setCsvFile')
-%     csvCheckbox = get(REMORA.bm_verify.csvCheckbox, 'string');
-%     REMORA.bm.settings.saveCsv = csvCheckbox;
+% elseif strcmp(action,'setXWAVdata')
+%     XWAVdataCheckbox = get(REMORA.mdLTSA_verify.XWAVCheckbox, 'string');
+%     REMORA.mdLTSA.settings.XWAVdata = XWAVdataCheckbox;
+% 
+% elseif strcmp(action,'setWAVdata')
+%     WAVdataCheckbox = get(REMORA.mdLTSA_verify.WAVCheckbox, 'string');
+%     REMORA.mdLTSA.settings.WAVdata = WAVdataCheckbox;
+
+elseif strcmp(action, 'setDataType')
+    dataType = get(REMORA.fig.mdLTSA.dataType_buttongroup.SelectedObject, 'Tag');
+    REMORA.mdLTSA.settings.dataType = dataType;
     
-elseif strcmp(action,'setXWAVdata')
-    XWAVdataCheckbox = get(REMORA.mdLTSA_verify.XWAVdataCheckbox, 'string');
-    REMORA.mdLTSA.settings.XWAVdata = XWAVdataCheckbox;
-
-elseif strcmp(action,'setWAVdata')
-    WAVdataCheckbox = get(REMORA.mdLTSA_verify.WAVdataCheckbox, 'string');
-    REMORA.mdLTSA.settings.WAVdata = wAVdataCheckbox;
-
 elseif strcmp(action,'RunBatchLTSA')
     close(REMORA.fig.mdLTSA.batch)
     mdLTSA_init_batch_detector;
