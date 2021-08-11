a6cdc01994d63cb93cc28664ffe8a0aecfde0860
sfregosi <22456126+sfregosi@users.noreply.github.com>
2021-08-11 14:24:31 -0400

    last bit of switch to batchLTSA naming

--- Remoras/MultiDirLTSAs/initialize.m
+++ Remoras/MultiDirLTSAs/initialize.m
@@ -1,15 +1,15 @@
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %
-% Initialize the MultiDirLTSA pulldown
+% Initialize the BatchLTSA pulldown
 %
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 global HANDLES REMORA PARAMS
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
-REMORA.mdLTSA.menu = uimenu(HANDLES.remmenu,'Label','&Batch LTSAs',...
+REMORA.batchLTSA.menu = uimenu(HANDLES.remmenu,'Label','&Batch LTSAs',...
     'Enable','on','Visible','on');
 
 % Batch create LTSAs over multiple directories
-uimenu(REMORA.mdLTSA.menu, 'Label', 'Batch create LTSAs', ...
-    'Callback', 'mdLTSA_pulldown(''batch_ltsas'')');
+uimenu(REMORA.batchLTSA.menu, 'Label', 'Batch create LTSAs', ...
+    'Callback', 'batchLTSA_pulldown(''batch_ltsas'')');
                    
