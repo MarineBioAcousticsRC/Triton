384d001ec32ffd4196620035999b1c6aae6ab04b
sfregosi <22456126+sfregosi@users.noreply.github.com>
2022-12-06 15:05:28 -0800

    added ability to use blanks to skip some dirs
    if you want to skip some dirs of xwavs/wavs nested within a larger dir, just change taves and dfreqs to blank in the parameter check GUI

--- Remoras/BatchLTSA/batchLTSA_mk_batch_ltsa.m
+++ Remoras/BatchLTSA/batchLTSA_mk_batch_ltsa.m
@@ -16,14 +16,14 @@ dfreqs =    PARAMS.ltsa.dfreqs;
 % loop through each of the sets of directories for actual ltsa creation
 for k = 1:length(indirs)
     lIdx = k; % which LTSA (For progress bar)
-        
+    
     PARAMS.ltsa.indir = char(indirs{k});
     PARAMS.ltsa.outdir = char(outdirs{k});
     PARAMS.ltsa.outfile = char(outfiles{k});
     PARAMS.ltsa.tave = taves(k);
     PARAMS.ltsa.dfreq = dfreqs(k);
-        
-    % run from matlab command line 
+    
+    % run from matlab command line
     if ~isfield(REMORA, 'hrp') % the *else* below isn't tested...carry over from Ann Allen*
         d = dirdata{k};
         if ~isfield(d, 'dataID') % non xwav/typical dir
@@ -38,8 +38,11 @@ for k = 1:length(indirs)
     end
     
     % make the ltsa!
+    disp_msg(sprintf('Creating LTSA %i/%i: %s.', lIdx, length(PARAMS.ltsa.indirs), ...
+        PARAMS.ltsa.indir));
     batchLTSA_mk_ltsa_dir(lIdx);
     fprintf('Finished LTSA for directory %s\n', PARAMS.ltsa.indir)
+    disp_msg(sprintf('Finished LTSA %i/%i.', lIdx, length(PARAMS.ltsa.indirs)));
     % fprintf('Press any key to continue...\n')
     %     pause
 end % all directories
--- Remoras/BatchLTSA/batchLTSA_mk_batch_ltsa_precheck.m
+++ Remoras/BatchLTSA/batchLTSA_mk_batch_ltsa_precheck.m
@@ -46,6 +46,8 @@ batchLTSA_chk_ltsa_params(indirs); % set taves and dfreqs
 if REMORA.batchLTSA.cancelled == 1; return; end
 taves = PARAMS.ltsa.taves;
 dfreqs = PARAMS.ltsa.dfreqs;
+indirs = PARAMS.ltsa.indirs;
+outdirs = PARAMS.ltsa.outdirs;
 
 % % raw files to skip.
 % % * this is specific to HRPs?
