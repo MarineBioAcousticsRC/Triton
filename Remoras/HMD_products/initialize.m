9d10dfe91dd421a258093835ec43635ddaac3d31
Vanessa ZoBell <53057768+vzobell@users.noreply.github.com>
2025-07-01 16:19:00 -0700

    Delete Remoras/HMD_products directory

--- Remoras/HMD_products/initialize.m
+++
@@ -1,9 +0,0 @@
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-% mypsd_init.m — Remora init function for 1-min daily PSD NetCDF writer
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-global HANDLES REMORA PARAMS
-
-% Add to Triton Remora menu
-REMORA.mypsd.menu = uimenu(HANDLES.remmenu, ...
-    'Label', 'Hybrid-millidecade Products', ...
-    'Callback', 'mypsd_run(''gui'')');  % opens GUI or folder select
--- Remoras/HMD_products/mypsd_run.m
+++
@@ -1,8 +0,0 @@
-function mypsd_run(option)
-    switch option
-        case 'gui'
-            mypsd_gui;
-        otherwise
-            error('Unknown option in mypsd_run: %s', option);
-    end
-end
