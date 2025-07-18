9b340c80ded465052b411ca5917c27b11b349e6b
Vanessa ZoBell <vmzobell@ucsd.edu>
2025-07-17 17:08:39 -0700

    deleting little things

--- Remoras/TriHyb/compute/mypsd_compute.m
+++ Remoras/TriHyb/compute/mypsd_compute.m
@@ -11,16 +11,10 @@ if ~exist(PARAMS.metadata.outputDir, 'dir')
 end
 
 disp('Compiling xwav files...')
-%getXWAVTimes_recur
-
-% get xwav directory to compute HMD
-%sm_get_xwav_dir_recur;
 
 % read data file headers
 sm_get_headers_recur;
 
-% check some ltsa parameters and other stuff
-%sm_ck_ltsaparams;
 
 % calculated averaged spectra
 profile on
