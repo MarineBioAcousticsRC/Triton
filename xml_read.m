5a7dce3f156add2d2d7f09774345049a58770b10
Kait Frasier <kfrasier@users.noreply.github.com>
2025-09-08 11:27:00 -0700

    Update xml read to fix crash on initialization

--- xml_read.m
+++ xml_read.m
@@ -93,9 +93,9 @@ RootName        = [];
 %% Check Matlab Version
 v = ver('MATLAB');
 version = str2double(regexp(v.Version, '\d.\d','match','once'));
-if (version<7.1)
-  error('Your MATLAB version is too old. You need version 7.1 or newer.');
-end
+% if (version<7.1)
+%   error('Your MATLAB version is too old. You need version 7.1 or newer.');
+% end
 
 %% read user preferences
 if (nargin>1)
