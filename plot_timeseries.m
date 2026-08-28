4dae8ff4d3bc675b95e6aedb55b0962dac87dd0e
sfregosi-noaa <188515456+sfregosi-noaa@users.noreply.github.com>
2026-08-28 07:10:09 +0000

    fix a few new plain flac bugs
    - ftype in picxwav could only be 1 or 2 so 'p' and 'n' didn't work for a folder of flac files
    - no PARAMS.raw.delimit_time existed so was throwing error on red line plot check

--- plot_timeseries.m
+++ plot_timeseries.m
@@ -48,8 +48,8 @@ else
 end
 
 % plot red line if plot figure crosses RawFile boundary & delimit button on
-% & not a wav file
-if PARAMS.ftype ~=1 && PARAMS.delimit.value && length(PARAMS.raw.delimit_time) ~= 1
+% & is xwav or xflac
+if PARAMS.ftype == 2 && PARAMS.delimit.value && length(PARAMS.raw.delimit_time) ~= 1
   for r=1:length(PARAMS.raw.delimit_time)
     y = [v(3),v(4)];
     x = [PARAMS.raw.delimit_time(r), PARAMS.raw.delimit_time(r)];
